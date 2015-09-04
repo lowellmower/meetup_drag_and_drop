a98904cc195b4cf0de5eba1c44ddd8504b5311aa
Lowell Mower <lowell.m.mower@gmail.com>
2015-09-04 15:56:11 -0400

    add get events method to controller for rMeetup gem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
 
   def index
+    @events ||= nil
+  end
+
+  def get_events
+    @events = Meetup.get_events
+    redirect root_path
   end
 
 end
