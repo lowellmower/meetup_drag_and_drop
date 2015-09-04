1f69f0a9a1b6c10a6f6ced3e96def1236d64c8c9
Lowell Mower <lowell.m.mower@gmail.com>
2015-09-04 19:36:46 -0400

    parse json resp for only name/url/count

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -10,17 +10,23 @@ class Meetup
     }
     meetup_api = MeetupApi.new
     events = meetup_api.open_events(params)
-    events
+    Meetup.filter_json(events)
+  end
+
+  def self.filter_json(response)
+    i = 0
+    results = []
+    while i < response["results"].length
+      event = {}
+      event["name"] = response["results"][i]["name"]
+      event["event_url"] = response["results"][i]["event_url"]
+      event["yes_rsvp_count"] = response["results"][i]["yes_rsvp_count"]
+      results << event
+      i+=1
+    end
+    results
   end
 
 end
 
 # "64685f47755242b72696f315757254"
-
-
-# RMeetup::Client.api_key = "64685f47755242b72696f315757254"
-# results = RMeetup::Client.fetch(:events,{:zip => "11205"})
-# # results.each do |result|
-
-# # end
-# results
